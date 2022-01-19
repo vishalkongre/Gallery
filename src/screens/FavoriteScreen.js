8faa6da237e2e922ecb1064e8c8ae148b386bf59
vishalkongre <vkongre92@gmail.com>
2022-01-19 15:09:16 +0530

    redme added

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,17 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
-import { useDispatch } from 'react-redux';
-
-import globalStyles from '../styles/global';
-import { Colors } from '../constants';
-import CustomButton from '../components/CustomButton';
-import Tasks from '../components/Tasks';
-import { getTasks } from '../store/actions/taskActions';
-import { setActiveListId } from '../store/actions/listAction';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import Favorites from '../components/Favorites';
 
 const FavoriteScreen = () => {
-    const [loading, setLoading] = useState(true);
     return (
         <View>
             <Favorites />
